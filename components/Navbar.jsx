2b0d17287f797c895e536d5fafc719683f57e8a7
Khalifa <khalifa.bihi351@campus.vinci.ac.ma>
2022-11-18 20:24:08 +0100

    deploy

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import Link from 'next/link';
-import { AiOutlineShopping } from 'react-icons/ai';
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+
+import { Cart } from "./";
+import { useStateContext } from "../context/StateContext";
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">
-          Khalifa Headphones
-        </Link>
+        <Link href="/">Khalifa Headphones</Link>
       </p>
 
-      <button type="button" className="cart-icon" onClick="">
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => setShowCart(true)}
+      >
         <AiOutlineShopping />
-        <span className="cart-item-qty">1</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
-    </div>
-  )
-}
 
-export default Navbar
+      {showCart && <Cart />}
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -12,7 +12,7 @@ import { client, urlFor } from "../../lib/client";
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const {decQty, incQty, qty} = useStateContext();
+  const {decQty, incQty, qty, onAdd} = useStateContext();
 
   return (
     <div>
@@ -66,7 +66,7 @@ const ProductDetails = ({ product, products }) => {
           </div>
 
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick="">
+            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>
               Add to Cart
             </button>
             <button type="button" className="buy-now" onClick="">
